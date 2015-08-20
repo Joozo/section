2fe2dd31ff57e858c5960eb0d8337a02d3495bf7
joozo <corp@joozo.cn>
2015-08-20 09:12:20 +0800

    modified 006 folder files

--- html/menu/002/js/docs.js
+++ html/menu/002/js/docs.js
@@ -12,15 +12,4 @@ $(function() {
         });
         $(".ct-dropdown-menu").css("display", "none");
     }
-
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= 40) {
-            if ($(document).height() - $(window).height() < 160) return;
-            $(".flex-menu .ct-navbar-nav > li > a").addClass("fixed-nav");
-            $(".flex-menu .ct-navbar-brand").addClass("fixed-brand");
-        } else {
-            $(".flex-menu .ct-navbar-nav > li > a").removeClass("fixed-nav");
-            $(".flex-menu .ct-navbar-brand").removeClass("fixed-brand");
-        }
-    });
 });
